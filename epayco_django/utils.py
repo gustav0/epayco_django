08602c1775c639d736dbda3effb978c1c71cb2ed
Gustavo Saavedra <tavito.286@gmail.com>
2020-06-26 21:10:52 -0500

    Agrega validaciones antes de consultar el ref_payco

--- epayco_django/utils.py
+++ epayco_django/utils.py
@@ -16,15 +16,39 @@ def get_signature(cust_id_client, p_key, ref_payco, transaction_id, amount, curr
 
 
 def validate_response_code(ref_payco, request=None):
+    """
+    Verifica el ref_payco. Funciona con los 2 tipos de ref_payco.
+
+    :param ref_payco:
+    :param request:
+    :return:
+    """
+
+    if not ref_payco:
+        return {'valid_ref': False}
+
     from .models import PaymentConfirmation
     options = {'apiKey': epayco_settings.PUBLIC_KEY,
                'privateKey': epayco_settings.PRIVATE_KEY,
                'test': epayco_settings.TEST,
                'lenguage': 'ES'}
+
+    response = None
+    if not ref_payco.isdigit():
+        response = requests.get('https://secure.epayco.co/validation/v1/reference/{}'.format(ref_payco))
+        try:
+            response = response.json()
+        except:
+            return {'valid_ref': False}
+        ref_payco = str(response['data']['x_ref_payco'])
+
     qs = PaymentConfirmation.objects.filter(ref_payco__iexact=ref_payco).exclude(cod_transaction_state=3)
     if not qs.exists():
-        epayco = Epayco.Epayco(options)
-        response = epayco.cash.get(ref_payco)  # Validate the reference
+        if not response:
+            epayco = Epayco.Epayco(options)
+            response = epayco.cash.get(ref_payco)
+
+        # Validate the reference
         if response['success'] == True:
             if epayco_settings.CONFIRMATION_URL.startswith('http'):
                 url = epayco_settings.CONFIRMATION_URL
