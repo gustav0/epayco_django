1e147253a452f5c85547d14a5fa30c63f19f8fb7
Gustavo Saavedra <tavito.286@gmail.com>
2021-02-20 21:42:05 -0500

    Update pip version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 
 setup(name='epayco-django',
-      version='0.0.8',
+      version='0.0.9',
       include_package_data=True,
       license='MIT',
       description="A django integration for ePayco's gateway.",
