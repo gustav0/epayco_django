ea71a3a48271ba87a501135ba2025c56279bf8fc
Gustavo Saavedra <tavito.286@gmail.com>
2021-09-17 19:01:35 -0500

    Adds validation to keys before saving a notification

--- epayco_django/utils.py
+++ epayco_django/utils.py
@@ -8,11 +8,10 @@ from .settings import epayco_settings
 
 def get_signature(cust_id_client, p_key, ref_payco, transaction_id, amount, currency_code):
     """
-        Genera el signature requerido por ePayco para verificar una confirmación.
+    Genera el signature requerido por ePayco para verificar una confirmación.
     """
-    signature = '{}^{}^{}^{}^{}^{}'.format(cust_id_client, p_key, ref_payco,
-                                           transaction_id, amount, currency_code)
-    return hashlib.sha256(signature.encode('utf')).hexdigest()
+    signature = "{}^{}^{}^{}^{}^{}".format(cust_id_client, p_key, ref_payco, transaction_id, amount, currency_code)
+    return hashlib.sha256(signature.encode("utf")).hexdigest()
 
 
 def validate_response_code(ref_payco, request=None):
@@ -25,22 +24,25 @@ def validate_response_code(ref_payco, request=None):
     """
 
     if not ref_payco:
-        return {'valid_ref': False}
+        return {"valid_ref": False}
 
     from .models import PaymentConfirmation
-    options = {'apiKey': epayco_settings.PUBLIC_KEY,
-               'privateKey': epayco_settings.PRIVATE_KEY,
-               'test': epayco_settings.TEST,
-               'lenguage': 'ES'}
+
+    options = {
+        "apiKey": epayco_settings.PUBLIC_KEY,
+        "privateKey": epayco_settings.PRIVATE_KEY,
+        "test": epayco_settings.TEST,
+        "lenguage": "ES",
+    }
 
     response = None
     if not ref_payco.isdigit():
-        response = requests.get('https://secure.epayco.co/validation/v1/reference/{}'.format(ref_payco))
+        response = requests.get("https://secure.epayco.co/validation/v1/reference/{}".format(ref_payco))
         try:
             response = response.json()
         except:
-            return {'valid_ref': False}
-        ref_payco = str(response['data']['x_ref_payco'])
+            return {"valid_ref": False}
+        ref_payco = str(response["data"]["x_ref_payco"])
 
     qs = PaymentConfirmation.objects.filter(ref_payco__iexact=ref_payco).exclude(cod_transaction_state=3)
     if not qs.exists():
@@ -49,25 +51,83 @@ def validate_response_code(ref_payco, request=None):
             response = epayco.cash.get(ref_payco)
 
         # Validate the reference
-        if response['success'] == True:
-            if epayco_settings.CONFIRMATION_URL.startswith('http'):
+        if response["success"] == True:
+            if epayco_settings.CONFIRMATION_URL.startswith("http"):
                 url = epayco_settings.CONFIRMATION_URL
             else:
-                url = '{}://{}{}'.format('https' if epayco_settings.FORCE_HTTPS or request.is_secure() else 'http',
-                                         request.get_host(), epayco_settings.CONFIRMATION_URL)
-            r2 = requests.post(url, data=response['data'])
+                url = "{}://{}{}".format(
+                    "https" if epayco_settings.FORCE_HTTPS or request.is_secure() else "http",
+                    request.get_host(),
+                    epayco_settings.CONFIRMATION_URL,
+                )
+            r2 = requests.post(url, data=response["data"])
             if r2.status_code == 405:
-                raise Exception('There seems the be an error reaching the confirmation URL.'
-                                ' Please make sure you are making good use of the "FORCE_HTTPS" setting.')
+                raise Exception(
+                    "There seems the be an error reaching the confirmation URL."
+                    ' Please make sure you are making good use of the "FORCE_HTTPS" setting.'
+                )
             obj = PaymentConfirmation.objects.filter(ref_payco__iexact=ref_payco).last()
             if obj:
-                return {'valid_ref': True, 'existed': False, 'flag': obj.is_flagged, 'obj': obj}
+                return {"valid_ref": True, "existed": False, "flag": obj.is_flagged, "obj": obj}
             else:
-                return {''}
-        return {'valid_ref': False}
+                return {""}
+        return {"valid_ref": False}
     elif qs.filter(flag=False).exists():
         obj = qs.filter(flag=False).last()
-        return {'valid_ref': True, 'existed': True, 'flag': False, 'obj': obj}
+        return {"valid_ref": True, "existed": True, "flag": False, "obj": obj}
     else:
         obj = qs.last()
-        return {'valid_ref': True, 'existed': True, 'flag': True, 'obj': obj}
+        return {"valid_ref": True, "existed": True, "flag": True, "obj": obj}
+
+
+def get_valid_keys():
+    return [
+        "cust_id_cliente",
+        "ref_payco",
+        "id_invoice",
+        "description",
+        "amount",
+        "amount_country",
+        "amount_ok",
+        "tax",
+        "amount_base",
+        "currency_code",
+        "bank_name",
+        "cardnumber",
+        "quotas",
+        "response",
+        "approval_code",
+        "transaction_id",
+        "transaction_date",
+        "cod_response",
+        "response_reason_text",
+        "errorcode",
+        "cod_transaction_state",
+        "transaction_state",
+        "franchise",
+        "business",
+        "customer_doctype",
+        "customer_document",
+        "customer_name",
+        "customer_lastname",
+        "customer_email",
+        "customer_phone",
+        "customer_movil",
+        "customer_ind_pais",
+        "customer_country",
+        "customer_city",
+        "customer_address",
+        "customer_ip",
+        "signature",
+        "test_request",
+        "extra1",
+        "extra2",
+        "extra3",
+        "extra4",
+        "extra5",
+        "extra6",
+        "extra7",
+        "extra8",
+        "extra9",
+        "extra10",
+    ]
--- epayco_django/views.py
+++ epayco_django/views.py
@@ -7,48 +7,51 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import View, TemplateView
 
 from .models import PaymentConfirmation
-from .utils import validate_response_code
+from .utils import get_valid_keys, validate_response_code
 
 
-@method_decorator(csrf_exempt, 'dispatch')
+@method_decorator(csrf_exempt, "dispatch")
 class ConfirmationView(View):
-
     def post(self, request, *args, **kwargs):
         data = {}
         for k, v in request.POST.items():
-            if k in ('x_id_factura', 'x_respuesta',
-                     'x_fecha_transaccion', 'x_cod_respuesta'):
+            if k in ("x_id_factura", "x_respuesta", "x_fecha_transaccion", "x_cod_respuesta"):
                 # Ignore fields in both English and Spanish
                 continue
-            data[k.replace('x_', '')] = v
+            data[k.replace("x_", "")] = v
         # TODO: Validate name consistency across all abstract models to prevent things like: id_user and customer_id.
         # Invert invoice name to keep the names consistent
-        data['invoice_id'] = data['id_invoice']
-        del data['id_invoice']
+        data["invoice_id"] = data["id_invoice"]
+        del data["id_invoice"]
 
         # Get boolean fron the test_request attribute
-        data['test_request'] = data['test_request'] == 'TRUE'
-        data['raw'] = json.dumps(request.POST)
+        data["test_request"] = data["test_request"] == "TRUE"
+        data["raw"] = json.dumps(request.POST)
+
+        # Validate there are no new fields.
+        # Epayco usually adds new fields without any notice.
+        valid_keys = get_valid_keys()
+        data = {k: v for k, v in data.items() if k in valid_keys}
 
         # The AbstractFlagSegment's model's save method does the flagging validations.
         item = PaymentConfirmation.objects.create(**data)
-        return JsonResponse({'flag': item.is_flagged}, status=200)
+        return JsonResponse({"flag": item.is_flagged}, status=200)
 
 
-@method_decorator(csrf_exempt, 'dispatch')
+@method_decorator(csrf_exempt, "dispatch")
 class ResponseValidationView(TemplateView):
-    template_name = 'simple_payment_response.html'
+    template_name = "simple_payment_response.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
-        ref = self.request.GET.get('ref_payco')
+        ref = self.request.GET.get("ref_payco")
         validation = validate_response_code(ref, self.request)
-        context['payment'] = validation
+        context["payment"] = validation
         return context
 
     def post(self, request, **kwargs):
-        ref = request.POST.get('x_ref_payco', None)
+        ref = request.POST.get("x_ref_payco", None)
         if ref is None:
-            return JsonResponse({'ref': ['This field is required.']}, status=400)
+            return JsonResponse({"ref": ["This field is required."]}, status=400)
         validate_response_code(ref, request)
-        return HttpResponseRedirect(reverse('epayco_response_validation') + '?ref_payco={}'.format(ref))
+        return HttpResponseRedirect(reverse("epayco_response_validation") + "?ref_payco={}".format(ref))
