9cd23cfc96b85fc53aacaed8466d4314057677d1
gustav0 <tavito.286@gmail.com>
2024-07-03 21:38:35 -0500

    feat: removes deprecated features Django4

--- epayco_django/urls.py
+++ epayco_django/urls.py
@@ -1,9 +1,18 @@
-from django.conf.urls import url
+try:
+    from django.urls import re_path
+except:
+    from django.conf.urls import url as re_path
 
 from .views import ConfirmationView, ResponseValidationView
 
 # app_name = 'epayco_django' # If i include this it will not find the urls.
 urlpatterns = [
-    url("^epayco/confirmation$", ConfirmationView.as_view(), name="epayco_confirmation"),
-    url("^payment/response-validation", ResponseValidationView.as_view(), name="epayco_response_validation"),
+    re_path(
+        "^epayco/confirmation$", ConfirmationView.as_view(), name="epayco_confirmation"
+    ),
+    re_path(
+        "^payment/response-validation$",
+        ResponseValidationView.as_view(),
+        name="epayco_response_validation",
+    ),
 ]
